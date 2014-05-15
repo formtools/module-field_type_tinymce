fd3c4f1ad0a2bda5551e9c21deb32656cdebb4c2
Ben Keen <ben.keen@gmail.com>
2014-05-14 23:29:00 -0700

    1.0.0

--- module.php
+++ module.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
- * Module file: TinyMCE Field
+ * Module file: TinyMCE
  */
 
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.0.8";
-$MODULE["date"]            = "2011-12-10";
+$MODULE["version"]         = "1.0.0";
+$MODULE["date"]            = "2011-05-17";
 $MODULE["origin_language"] = "en_us";
 
 
